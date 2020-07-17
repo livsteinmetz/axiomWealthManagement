a3f024acdd24d44d18c85b6e0ee6a9b4b49d0687
Olivia Steinmetz <livsteinmetz@Olivias-MacBook-Air.local>
2020-07-17 12:19:37 -0400

    updating content

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from "react";
-import Header from "./Components/Header/Header";
-import Landing from "./Components/Landing/Landing";
-import Footer from "./Components/Footer/Footer";
+import React from 'react'
+import Header from './Components/Header/Header'
+import Landing from './Components/Landing/Landing'
+import Footer from './Components/Footer/Footer'
+import About from './Components/About/About'
+
 export default function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Landing />
-      <Landing />
-      <Landing />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<Landing />
+			<About />
+			<Footer />
+		</div>
+	)
 }
---
+++ src/Components/About/About.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import './About.scss'
+
+export default function About() {
+	return (
+		<div id="About">
+			<h2>Planning for transitions in life.</h2>
+
+			<div id="AboutAxiom">
+				<img src="/pillars_white.png" alt="Pillars" />
+				<div id="Definition">
+					<h3>Axiom</h3>
+					<h4>an established rule, principle or self-evident truth</h4>
+					<h4>a principle or foundation upon which all else is built</h4>
+					<p>
+						At Axiom Wealth Management, it’s not just business, it’s personal.
+						Solid personal relationships and sound financial practices are the
+						pillars upon which successful relationships are built and goals are
+						achieved. My average relationship exceeds 19 years and, for more
+						than half my client base, includes two or more generations of their
+						families. I help make financial goals clear and attainable, families
+						financially secure and dreams a reality. My axioms are:
+					</p>
+					<ul id="AxiomList">
+						<li>Honesty and integrity are simply unquestionable.</li>
+						<li>As a fiduciary, always act in the clients' best interest.</li>
+						<li>
+							Know the client; clearly understand current and evolving needs.
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div id="AboutSethMcHenry">
+				<img src="/SethMcHenry.jpg" alt="SethMcHenry" />
+				<div>
+					<h3>Seth McHenry, CRPC</h3>
+					<h4>LPL Financial Advisor</h4>
+					<h4>LPL Registered Principal</h4>
+					<p>
+						Seth, a 29-year veteran of the financial services industry, and his
+						wife Nancy live in Yardley, PA where as fourth-generation Bucks
+						County residents they raised their three children Scott, Shannon and
+						Ryan. Over the past 30 years, Seth has coached many local youth
+						sports teams and served in leadership positions and or board member
+						roles for his all children’s activities. As a Life Member of the
+						National Eagle Scout Association, he has served as a Boy Scout
+						leader and currently serves his church as the president of the board
+						of trustees, chair of the finance committee, as a deacon and in
+						various other roles. In his spare time, Seth can be found reading,
+						performing vocal music, swimming, running with his dogs, and
+						enjoying weekends at the beach in Barnegat Light, NJ. His
+						professional biography is outlined below.
+					</p>
+				</div>
+			</div>
+		</div>
+	)
+}
